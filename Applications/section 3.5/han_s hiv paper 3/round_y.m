ea092a6ba3fe90de7834c27dd8db5564faf55c0f
Elvis Cui <44211579+ElvisCuiHan@users.noreply.github.com>
2023-06-26 09:39:25 -0700

    Delete Applications/section 3.5/han_s hiv paper 3 directory

--- Applications/section 3.5/han_s hiv paper 3/round_y.m
+++
@@ -1,9 +0,0 @@
-function new_y = round_y(y, epsilon)
-    lower_counts = floor(y / epsilon);
-    upper_counts = ceil(y / epsilon);
-    lower_ints = lower_counts .* epsilon;
-    upper_ints = upper_counts .* epsilon;
-    logic = abs(lower_ints - y) < abs(upper_ints - y);
-    new_y = logic .* lower_ints + (1 - logic) .* upper_ints;
-end
- 
